bd0467374e5f3833b1e29d0e085cc925ac5aef32
vanai <vanaiinc@gmail.com>
2025-02-10 22:34:59 -0800

    feat: add openpanel init

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { OpenPanelComponent } from "@openpanel/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,14 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
       </body>
+      <OpenPanelComponent
+        clientId="cd47364b-1a45-40bf-831e-5255e2e1fe4f"
+        trackScreenViews={true}
+        // trackAttributes={true}
+        // trackOutgoingLinks={true}
+        // If you have a user id, you can pass it here to identify the user
+        // profileId={'123'}
+      />
       <SpeedInsights />
       <Analytics />
     </html>
